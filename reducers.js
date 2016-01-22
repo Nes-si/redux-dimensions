0d62a9e47a0ba4ede880d69aa34e39a5ec52d2e7
Никита Головин <golovin.nik@gmail.com>
2016-01-23 01:20:58 +0300

    actions, reducers. 2

--- reducers.js
+++ reducers.js
@@ -1,18 +1,6 @@
 import { combineReducers } from 'redux';
 import { CALC } from './actions';
 
-function calc(state = [], action) {
-    switch (action.type) {
-        case CALC:
-            return [...state, {
-                text: action.text,
-                completed: false
-            }];
-
-        default:
-            return state;
-    }
-}
 
 function calc(state = {width: 1, height: 1, depth: 1}, action) {
     switch (action.type) {
